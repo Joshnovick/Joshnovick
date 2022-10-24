2098453ac7bbc00253c9136b85b516d1ef5b3ac3
Joshua Novick <joshnovick@outlook.com>
2022-10-23 23:22:28 -0600

    created assignment 9

--- js/assignment9.js
+++ js/assignment9.js
@@ -4,32 +4,49 @@ const thumbBar = document.querySelector(".thumb-bar");
 const btn = document.querySelector("button");
 const overlay = document.querySelector(".overlay");
 
-
-
 /* Declaring the array of image filenames */
-const pix = ["pic1.jpg", "pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"];
+
+const pix = ["berlin.png", "copenhagen.png", "finland.png", "hikingPic.jpg", "josh.jpg"];
 
 /* Declaring the alternative text for each image file */
-const alt = {"pic1.jpg":"Closeup of a blue human eye",
-            "pic2.jpg":"A painted wave",
-            "pic3.jpg":"Purple and white flowers",
-            "pic4.jpg":"Egyptian Art",
-            "pic5.jpg":"Closeup of a brown butterly"};
+
+const alt = {"berlin.png":"A picture of me in Berlin",
+            "copenhagen.png":"A picture of me in Copenhagen",
+            "finland.png":"A picture of me in Finland",
+            "hikingPic.jpg":"A picture of me hiking",
+            "josh.jpg":"My LinkedIn profile pic"};
 
 /* Looping through images */
+
 let path = "xxx"
 for (const image of pix) {
     console.log(image);
     const newImage = document.createElement('img');
-    path = "../../img/ica9/assignment9/" + image;
+    path = "../../img/index/" + image;
     console.log(path);
     newImage.setAttribute('src', path);
     newImage.setAttribute('alt', alt[image]);
     thumbBar.appendChild(newImage);
-    };
-
-
-
-
+    newImage.addEventListener("click", e => {
+        displayedImage.src = e.target.src;
+        displayedImage.alt = e.target.alt;
+    });
+};
 
 /* Wiring up the Darken/Lighten button */
+
+btn.addEventListener('click', () => {
+    const btnClass = btn.getAttribute('class');
+    
+    if (btnClass === 'dark') {
+      btn.setAttribute('class','light');
+      btn.textContent = 'Lighten';
+      overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+    } 
+    
+    else {
+      btn.setAttribute('class','dark');
+      btn.textContent = 'Darken';
+      overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+    }
+  });
